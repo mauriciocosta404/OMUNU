cda29703bd708aaf39864030f974360a1b94d495
menezescosta <mc611641@gmail.com>
2023-02-06 14:47:34 +0100

    feat: adicionando o componente bookCategory

---
+++ src/components/bookCategory/index.tsx
@@ -0,0 +1,15 @@
+import * as S from './styles'
+import React from 'react'
+import { BookCategoryType } from './type'
+
+const BookCategory:React.FC<BookCategoryType> =({
+    title,
+    Icon
+})=>{
+    return(
+        <S.Container>
+            <span>{title}</span>
+            <Icon/>
+        </S.Container>
+    )
+}
---
+++ src/components/bookCategory/type.ts
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export type BookCategoryType={
+    title:string,
+    Icon: React.ReactNode
+}
