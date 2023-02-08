b37b9590956c58eac8a993bda98c190ef7a231c5
menezescosta <mc611641@gmail.com>
2023-02-08 16:58:38 +0100

    feat: adicionando o conteudo

--- src/views/about/index.tsx
+++ src/views/about/index.tsx
@@ -1,9 +1,22 @@
+import Logo from '../../components/logo';
 import * as S from './styles'
 
 const About=()=>{
     return(
         <S.Container>
-            About
+            <Logo/>
+            <S.Title>
+
+            </S.Title>
+            <S.Content>
+                <S.Description>
+                    Entra na onda e descobre a magia infantil com frases e livros para alunos 
+                    vinculado no âmbito da educação para o desenvolvimento de escrituras dando
+                    assim aberturas para leitores infantis no contexto e nos exercícios propostos
+                    na educação infantil, com a finalmente de criar crianças com incentivo de 
+                    inovação no processo da educação.
+                </S.Description>
+            </S.Content>
         </S.Container>
     )
 }
--- src/views/about/styles.ts
+++ src/views/about/styles.ts
@@ -3,3 +3,15 @@ import styled from "styled-components";
 export const Container=styled.div`
 
 `;
+
+export const Title=styled.h2`
+
+`;
+
+export const Content=styled.div`
+
+`;
+
+export const Description=styled.p`
+    color:#385A64;
+`;
