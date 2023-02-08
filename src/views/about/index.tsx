e789dce08955dc12668f61b10a73c8ab54ceea91
menezescosta <mc611641@gmail.com>
2023-02-08 17:06:32 +0100

    style: adicionando a borda do content

--- src/views/about/index.tsx
+++ src/views/about/index.tsx
@@ -1,12 +1,14 @@
-import Logo from '../../components/logo';
+import Logo from '../../components/logo'
 import * as S from './styles'
+import Image from 'next/image'
+import aboutImage from '../../assets/images/about.png'
 
 const About=()=>{
     return(
         <S.Container>
             <Logo/>
             <S.Title>
-
+                Sobre
             </S.Title>
             <S.Content>
                 <S.Description>
@@ -16,6 +18,7 @@ const About=()=>{
                     na educação infantil, com a finalmente de criar crianças com incentivo de 
                     inovação no processo da educação.
                 </S.Description>
+                <Image src={aboutImage} alt=''/>
             </S.Content>
         </S.Container>
     )
--- src/views/about/styles.ts
+++ src/views/about/styles.ts
@@ -5,11 +5,14 @@ export const Container=styled.div`
 `;
 
 export const Title=styled.h2`
-
+    color:#385A64;
 `;
 
 export const Content=styled.div`
-
+    display:flex;
+    align-items: center;
+    border: 1px solid #CFCECE;
+    border-radius: 1rem;
 `;
 
 export const Description=styled.p`
